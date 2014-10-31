ea5fab878d193fed067426b9975d2313d40dcc6d
Joel Junström <joel.junstrom@oktavilla.se>
2014-10-31 13:20:19 +0100

    Spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+if ENV["CI"]
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
+RSpec.configure do |config|
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+end
