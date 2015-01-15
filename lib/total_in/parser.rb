0488c0d680806cfb55a34aa94d727115d7279ffd
Joel Junström <joel.junstrom@oktavilla.se>
2015-01-15 16:47:32 +0100

    Encode each line in utf-8
    We can now receive files in multiple encodings (that can be coerced to
    utf-8). Do not try to encode lines if already in utf-8
    
    TotalIn::Parser now accepts either a string or a File object.

--- lib/total_in/parser.rb
+++ lib/total_in/parser.rb
@@ -6,36 +6,46 @@ module TotalIn
   class InvalidFileFormatError < ArgumentError; end;
 
   class Parser
-    attr_reader :text
+    attr_reader :file
 
-    def initialize text
-      validate_file_format text.each_line.peek
-      @text = text
+    # Parser.new accepts a File instance or a String
+    # A InvalidFileFormatError will be raised if file isn't in the TotalIn format
+    def initialize file
+      @file = file
+      validate_file_format
     end
 
     def result
-      contexts = parse_lines text.each_line, Contexts.new
+      parse_lines(Contexts.new).result
+    end
 
-      contexts.result
+    protected
+
+    def lines
+      @lines ||= file.each_line
     end
 
     private
 
-    def validate_file_format line
-      validator = FileFormatValidator.new line
-
+    def validate_file_format
+      validator = FileFormatValidator.new first_line
       raise InvalidFileFormatError.new(validator.errors.join(", ")) unless validator.valid?
     end
 
-    def parse_lines lines, contexts
+    def parse_lines contexts
       begin
-        parse_lines lines, parse_line(lines.next, contexts)
+        parse_lines parse_line(self.lines.next, contexts)
       rescue StopIteration
+        self.lines.rewind # Ensure we do not bomb out when calling result multiple times
         contexts
       end
     end
 
+    # Look up a matching handler for the line and process it
+    # The process method on a handler always returns a Contexts object
     def parse_line line, contexts
+      line = line.encode Encoding::UTF_8 if encode_lines?
+
       handler = handler_for_line line
 
       handler.process line, contexts
@@ -43,6 +53,19 @@ module TotalIn
 
     def handler_for_line line
       LineHandlers.mapping.fetch line[0..1]
+    end
+
+    def encode_lines?
+      first_line.encoding != Encoding::UTF_8
+    end
+
+    def first_line
+      @first_line ||= begin
+        line = self.lines.peek
+        self.lines.rewind # peek seems to move the pointer when file is an actual File object
+
+        line
+      end
     end
   end
 end
